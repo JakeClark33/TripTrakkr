b7719e087c3ff7e02a95cc2fdccabdb9d27a0ea3
JP Mamuric <mamuricj@gmail.com>
2021-09-06 01:34:18 -0500

    updated frontend framework

--- index.js
+++ index.js
@@ -4,5 +4,20 @@ const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 
 burgerIcon.addEventListener('click', () => {
-    navbarMenu.classList.toggle('is-active')
+    navbarMenu.classList.toggle('is-active');
+})
+
+// modal and sign-in
+
+const signin = document.getElementsByTagName('#signin');
+const modalBg = document.querySelector('.modal-background');
+const modal = document.query.selector('.modal');
+
+signin.addEventListener('click',() => { 
+    modal.classlist.add('is-active');
+
+})
+
+signin.addEventListener('click',() => {
+    modal.classlist.remove('is-active');
 })
