7ed30a2d7bbc6cc3b39e8718e71f90faf97fc386
JP Mamuric <mamuricj@gmail.com>
2021-09-05 22:54:51 -0500

    added js file to enteract with front end

---
+++ index.js
@@ -0,0 +1,8 @@
+// mobile menu for when the sizing doesn't show the text
+
+const burgerIcon = document.querySelector('#burger');
+const navbarMenu = document.querySelector('#nav-links');
+
+burgerIcon.addEventListener('click', () => {
+    navbarMenu.classList.toggle('is-active')
+})
