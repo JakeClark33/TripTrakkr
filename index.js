234a1932d7a2fef6a2d012e897b0f8a10fb1ed14
YourMajestyKing <carolynking213@gmail.com>
2021-09-06 19:40:51 -0500

    fixing signin modal

--- index.js
+++ index.js
@@ -4,24 +4,21 @@ const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 
 burgerIcon.addEventListener('click', () => {
+    console.log(navbarMenu.classList);
     navbarMenu.classList.toggle('is-active');
-})
+});
 
 // modal and sign-in
 
 const signinbutton = document.querySelector('#signin');
 const modalBg = document.querySelector('.modal-background');
 const modal = document.querySelector('.modal');
+const modalClose = document.querySelector('.modal-close');
 
 signinbutton.addEventListener('click', () => { 
-    modal.classlist.add('is-active');
-    Box.classlist.remove('is-active');
+    modal.classList.add('is-active');
+});
 
-})
-
-console.log("#signin");
-
-signinbutton.addEventListener('click',() => {
-    modal.classlist.remove('is-active');
-    box.classlist.toggle('is-active');
-})
+modalClose.addEventListener('click', ()=> {
+    modal.classList.remove('is-active');
+});
