3f1150123d70b3fb456410b5e6c552e3749d2af2
JakeClark33 <jakemclark@hotmail.com>
2021-09-06 20:38:07 -0500

    updated

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 const hbs = exphbs.create({});
+const sequelize = require('./connection/connections');
+
+
+
+const app = require('express')();
+const PORT = process.env.PORT || 3001;
 
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.set('views', routes);
+
+// app.Router().use('/', routes);
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+  });
+  
