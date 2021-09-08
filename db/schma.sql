c316c126d6e39762af44f762d505ad3127a223c1
Apo11o <jeffknoespel@gmail.com>
2021-09-07 19:39:56 -0500

    added rows to landmarks table

--- db/schma.sql
+++ db/schma.sql
@@ -3,6 +3,9 @@ DROP TABLE IF EXISTS favorites;
 
 CREATE TABLE landmarks (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    landmark_name VARCHAR(50),
+    landmark_description TEXT,
+    landmark_location VARCHAR(30)
 );
 
 CREATE TABLE favorites (
