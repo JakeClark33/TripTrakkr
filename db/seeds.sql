cb966f0c6a0fa6f362575c9b40e7e3b32f18355d
Apo11o <jeffknoespel@gmail.com>
2021-09-07 22:07:07 -0500

    changed connection.js to sequelize/env type

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,8 @@ INSERT INTO landmarks (landmark_name, landmark_location, landmark_description)
 VALUES
     ('Lambeau Field', 'Green Bay, WI', 'Home of the Green Bay Packers!'),
     ('Highway 42', 'Northport, WI', 'A twisty road at the tip of Door County is the last streach before taking the ferry to Washington Island.'),
-    ('Wisconsin State Capitol', 'Madison, WI', 'The beautiful domed home of the Wisconsin Stat Government');
+    ('Wisconsin State Capitol', 'Madison, WI', 'The beautiful domed home of the Wisconsin Stat Government.'),
+    ('Milwaukee Art Museum', 'Milwaukee, WI', 'Iconic architecture, incredible culture.');
 
 INSERT INTO favorites (landmark_name, is_favorite)
 VALUES
