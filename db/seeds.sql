179bfd3b81fe8e461b9ba807392388ca035b919a
Apo11o <jeffknoespel@gmail.com>
2021-09-07 20:57:32 -0500

    additional seeding

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,11 @@
 INSERT INTO landmarks (landmark_name, landmark_location, landmark_description)
 VALUES
     ('Lambeau Field', 'Green Bay, WI', 'Home of the Green Bay Packers!'),
+    ('Highway 42', 'Northport, WI', 'A twisty road at the tip of Door County is the last streach before taking the ferry to Washington Island.'),
     ('Wisconsin State Capitol', 'Madison, WI', 'The beautiful domed home of the Wisconsin Stat Government');
 
 INSERT INTO favorites (landmark_name, is_favorite)
 VALUES
     ('Lambeau Field', 1),
-    ('Second name', 0);
+    ('Highway 42', 0),
+    ('Wisconsin State Capitol', 0);
