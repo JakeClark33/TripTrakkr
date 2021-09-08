1a220e1e7d272951054dd2b052cd426f2ba38900
Apo11o <jeffknoespel@gmail.com>
2021-09-07 19:52:37 -0500

    added seed framework

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO landmarks (landmark_name, landmark_description, landmark_location)
+VALUES
+    ('First location', 'First description', 'First location'),
+    ('Second location', 'Second description', 'Second location');
+
+INSERT INTO favorites (landmark_name, is_favorite)
+VALUES
+    ('First name', 1),
+    ('Second name', 0);
