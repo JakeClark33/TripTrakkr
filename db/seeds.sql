b1b27810647c2a793734ed961e68cb7565b3ab2b
Apo11o <jeffknoespel@gmail.com>
2021-09-07 20:30:34 -0500

    seeded tables

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
 INSERT INTO landmarks (landmark_name, landmark_location, landmark_description)
 VALUES
-    ('First location', 'First location', 'First description'),
-    ('Second location', 'Second location', 'Second description');
+    ('Lambeau Field', 'Green Bay, WI', 'Home of the Green Bay Packers!'),
+    ('Wisconsin State Capitol', 'Madison, WI', 'The beautiful domed home of the Wisconsin Stat Government');
 
 INSERT INTO favorites (landmark_name, is_favorite)
 VALUES
-    ('First name', 1),
+    ('Lambeau Field', 1),
     ('Second name', 0);
