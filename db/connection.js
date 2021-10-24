e226b6587706639248f01811948ab590ccadfed7
Apo11o <jeffknoespel@gmail.com>
2021-10-24 14:13:39 -0500

    changed host

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
+    host: 'en1ehf30yom7txe7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     dialect: 'mysql',
     port: 3306
 });
